62ed099a81d4127a43e3a0e71f12a1cef251a795
senmay <dominik.szarzynski@gmail.com>
2025-08-01 20:44:43 +0200

    fixingh route

--- backend/main.py
+++ backend/main.py
@@ -30,6 +30,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://geoai.mapiarz.pl",
+        "https://mapiarz.pl",
+        "https://geoai.mapiarz.pl",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
     ],
